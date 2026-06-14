5f04bb4b319f89b9bc7b7c97c5e7a8456787f4e7
Johnny Nguyen <jng006@uib.no>
2026-06-14 16:19:53 +0200

    rm api endpoint

--- backend/server.js
+++ backend/server.js
@@ -10,16 +10,6 @@ app.use(express.json())
 
 
 // routes
-app.get("/videos3/songs", async (req, res) => {
-    try {
-        const allData = await pool.query("SELECT DISTINCT songname, creator FROM videos3 ORDER BY songname");
-        res.json(allData.rows);
-    } catch (err) {
-        console.error(err.message);
-    }
-});
-
-
 app.get("/kartpop", async (req, res) => {
     try {
         const allData = await pool.query("SELECT * FROM komlatlong");
